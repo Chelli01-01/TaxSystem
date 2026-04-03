6284b536500be892d991dca6b4bd31b019795ffa
Chelli01-01 <chelli4and22@gmail.com>
2026-04-03 18:47:44 +0400

    Implemented TaxSystem Logic

--- src/com/tax/math/TaxSystemLogic.java
+++ src/com/tax/math/TaxSystemLogic.java
@@ -5,9 +5,10 @@ public class TaxSystemLogic {
 	public static void calculate(TaxRecord record) {
 		//A. Calculation of Taxable Income
 		
-		double taxableAmount= record.getAnnualIncome()-(record.getDepAllowance()+record.getMedAllowance());
+		double taxableAmount = record.getAnnualIncome()- (record.getDepAllowance()+record.getMedAllowance());
 		
 		//B. MRA 2026 Rule: if taxable I< 500,000; tax=0%, else 10% Flat Rate
+		// Also tackling negative Taxable Income
 		
 		if (taxableAmount<500000 || taxableAmount<0) {
 			record.setTaxPayable(0.0);
